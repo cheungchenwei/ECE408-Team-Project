b89cd85e606792da7c9090db7ce1f011c2a958e5
Rick <654834341@qq.com>
2018-11-30 16:29:39 -0600

    Loop unrolling
    Op Time: 0.053147
    Op Time: 0.133266

--- ece408_project/ece408_src/new-forward.cuh
+++ ece408_project/ece408_src/new-forward.cuh
@@ -75,6 +75,76 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #undef k4d
 }
 
+__global__ void forward_kernel_unroll(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
+{
+
+    /*
+    Modify this function to implement the forward pass described in Chapter 16.
+    We have added an additional dimension to the tensors to support an entire mini-batch
+    The goal here is to be correct AND fast.
+    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
+    */
+
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+
+// An example use of these macros:
+// float a = y4d(0,0,0,0)
+// y4d(0,0,0,0) = a
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+    int W_grid = ceil(W_out / (1.0 * TILE_WIDTH));
+     
+
+    //int n = blockIdx.x * 2;
+     int n = blockIdx.x;  
+    //int m = blockIdx.y * 2;
+     int m = blockIdx.y;
+    
+    int h = ((blockIdx.z / W_grid) * TILE_WIDTH) + threadIdx.y;
+    int w = ((blockIdx.z % W_grid) * TILE_WIDTH) + threadIdx.x;
+    
+    float acc = 0.0;
+
+	
+	if (n < B && m < M && h < H_out && w < W_out){
+
+            /*
+            Optimization: convolution loop unrolling, using "#pragma unroll"
+            Op Time: 0.053147
+            Op Time: 0.133266
+            Reason: Since the kernel size is fixed(7), we can use loop unrolling to transfer dynamic loop into static loop.
+            Loop unrolling is effective due to the memory access in computer.
+            For example, "for(int i = 0; i < 2; i++) b[i] += 1" is slower than "b[0] += 1; b[1] += 1;". This is because in 
+            the former version, variable i increases 2 times and assign to b 2 times, whereas the latter version only increase i by 2.
+            */
+            
+        for( int c = 0; c < C; c++){
+            #pragma unroll 7
+            for( int p = 0; p < K; p++){
+                #pragma unroll 7
+                for( int q = 0; q < K; q++){
+                    acc += x4d(n, c,  h + p, w + q) * k4d(m, c, p, q);
+                
+                }
+            }
+        }
+            
+        
+
+        y4d(n, m, h, w) = acc;
+
+
+	}
+
+
+#undef y4d
+#undef x4d
+#undef k4d
+}
+
 /* 
    This function is called by new-inl.h
    Any code you write should be executed by this function.
@@ -97,7 +167,6 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int H = x.shape_[2];
     const int W = x.shape_[3];
     const int K = w.shape_[3];
-
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
     int W_grid = ceil(W_out / (1.0 * TILE_WIDTH));
@@ -106,12 +175,13 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Set the kernel dimensions
     
-    // dim3 gridDim(B, M, Z);
-    dim3 gridDim(ceil(B / 2.0), ceil(M / 2.0), Z);
+     dim3 gridDim(B, M, Z);
+    //dim3 gridDim(ceil(B / 2.0), ceil(M / 2.0), Z);
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
 
     // Call the kernel
-    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B, M, C, H, W, K);
+    //forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B, M, C, H, W, K);
+    forward_kernel_unroll<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B, M, C, H, W, K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
